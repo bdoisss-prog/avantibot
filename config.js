940a4fbe03a08e004b865b182d1c4397ad1cce04
bdoisss-prog <bdoisss@gmail.com>
2026-02-08 11:04:33 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -4,10 +4,28 @@ export default {
 
     // Configurações de Busca
     search: {
+        useGoogleNews: false, // Definido como false para buscar APENAS dos links abaixo
         query: "Palmeiras",
         lang: "pt-BR",
         country: "BR",
-        ceid: "BR:pt"
+        ceid: "BR:pt",
+        // Lista exclusiva de feeds solicitada
+        additionalRssFeeds: [
+            "https://ptd.verdao.net/feed/",
+            "https://nossopalestra.com.br/feed/",
+            "https://palmeirasonline.com/feed/",
+            "https://www.mondopalmeiras.net/feed/",
+            "https://anythingpalmeiras.com/feed/",
+            "https://arquivospalmeiras.blogspot.com/feeds/posts/default?alt=rss",
+            "https://se-palmeiras.webnode.page/rss/all.xml",
+            "https://se-palmeiras.webnode.page/rss/noticias.xml",
+            "https://palmeiras-noticias5.webnode.page/rss/all.xml",
+            "https://palmeiras-noticias5.webnode.page/rss/noticias.xml",
+            "https://palmeiras-noticias5.webnode.page/rss/blog.xml",
+            "https://tudopalmeiras.webnode.page/rss/all.xml",
+            "https://tudopalmeiras.webnode.page/rss/brasileir%c3%a3o%202008.xml",
+            "https://tudopalmeiras.webnode.page/rss/links-relacionados.xml"
+        ]
     },
 
     // Configurações de Intervalo
